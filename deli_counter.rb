e03e75f0fdf4a73028421ebd4ad9634638ab2a8b
Chett Tiller <chett.tiller@gmail.com>
2018-09-22 12:00:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,9 @@
 def line(line)
   return "The line is currently empty" if line.length = 0
-  string = "The line is currently "
+  string = "The line is currently: "
+  line.each do |name|
+    
+  end
 end
 
 def take_a_number
