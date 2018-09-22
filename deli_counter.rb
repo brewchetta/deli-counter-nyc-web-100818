de836978736279f53e98ebe16c494125af2ffa65
Chett Tiller <chett.tiller@gmail.com>
2018-09-22 12:13:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,17 @@
 require 'pry'
 
 def line(line)
-  puts "The line is currently empty" if line.length == 0
-  break
-  string = "The line is currently: "
-  i = 1
-  line.each do |name|
-    string += i.to_s + ". " + name + " "
-    i += 1
+  if line.length == 0
+    puts "The line is currently empty"
+  else
+    string = "The line is currently: "
+    i = 1
+    line.each do |name|
+      string += i.to_s + ". " + name + " "
+      i += 1
+    end
+    puts string.strip
   end
-  puts string.strip
 end
 
 def take_a_number(line,name)
